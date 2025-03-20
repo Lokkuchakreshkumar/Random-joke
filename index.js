d8ebe4975ffaa3530dab2ebfbe681df997064f9b
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2025-03-20 19:31:06 +0530

    added ui elements

--- index.js
+++ index.js
@@ -4,40 +4,42 @@ let config = {
     Accept: "application/json",
   },
 };
+getJoke();
+p = document.createElement("p");
+let emop = document.createElement("p");
 async function getJoke() {
   let response = await axios.get(url, config);
   let data = response.data;
   let joke = data.joke;
-  cleardiv();
+  // cleardiv();
   putjoke();
   printJoke(joke);
 }
 function printJoke(joke) {
-  let p = document.createElement("p");
   p.innerText = "";
   p.innerText = joke;
+  p.className = "name";
   let div = document.querySelector(".container");
   div.appendChild(p);
 }
-let emojis = ["😁", "😂", "😉", "😊", "🤣", "😆", "☺️", "😹", "😜", "🤭"];
+let emojis = ["😁", "😂", "😉", "😊", "🤣", "😆", "☺️", "😹", "😜", "🤭", "🤪"];
 function getrandomEmoji() {
   let random = Math.floor(Math.random() * emojis.length);
   return random;
 }
 function putjoke() {
   let randomNumber = getrandomEmoji();
-  let p = document.createElement("p");
-  p.className = "emoji";
-  p.innerText = "";
-  p.innerText = emojis[randomNumber];
+  emop.className = "emoji";
+  emop.innerText = "";
+  emop.innerText = emojis[randomNumber];
   let div = document.querySelector(".container");
-  div.appendChild(p);
+  div.appendChild(emop);
 }
 btn = document.querySelector("button");
 btn.addEventListener("click", () => {
   getJoke();
 });
-function cleardiv() {
-  let container = document.querySelector(".container");
-  container.innerHTML = "";
-}
+// function cleardiv() {
+//   let container = document.querySelector(".container");
+
+// }
